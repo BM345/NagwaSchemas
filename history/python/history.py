c5315207f4f7659081d5292036b59e3f939ba8b0
BM345 <40637208+BM345@users.noreply.github.com>
2021-05-21 14:23:23 +0100

    Added more to history code

--- history/python/history.py
+++ history/python/history.py
@@ -122,4 +122,100 @@ class Action(object):
 
 class CreateEntityAction(Action):
     def __init__(self, takenAt, takenBy):
-        super(CreateEntityAction, self).__init__(takenAt, takenBy, "created_entity")
+        super().__init__(takenAt, takenBy, "created_entity")
+
+class CreateNewVersionAction(Action):
+    def __init__(self, takenAt, takenBy, reference, fileName):
+        super().__init__(takenAt, takenBy, "created_new_version")
+
+        self.reference = reference 
+        self.fileName = fileName 
+
+    def toXML(self):
+        e1 = super().toXML()
+
+        e2 = XMLSubelement(e1, "version")
+        e2.text = self.reference 
+
+        e3 = XMLSubelement(e1, "file_name")
+        e3.text = self.fileName 
+
+        return e1 
+
+class ChangeWorkflowAction(Action):
+    def __init__(self, takenAt, takenBy, newWorkflowReference):
+        super().__init__(takenAt, takenBy, "changed_workflow")
+
+        self.newWorkflowReference = newWorkflowReference 
+
+    def toXML(self):
+        e1 = super().toXML()
+
+        e2 = XMLSubelement(e1, "new_workflow")
+        e2.text = self.newWorkflowReference 
+
+        return e1 
+
+class ChangeWorkflowStatusAction(Action):
+    def __init__(self, takenAt, takenBy, transition, newWorkflowStatusReference):
+        super().__init__(takenAt, takenBy, "changed_workflow_status")
+
+        self.transition = transition
+        self.newWorkflowStatusReference = newWorkflowStatusReference 
+
+    def toXML(self):
+        e1 = super().toXML()
+
+        e2 = XMLSubelement(e1, "transition")
+        e2.text = self.transition 
+
+        e3 = XMLSubelement(e1, "new_workflow_status")
+        e3.text = self.newWorkflowStatusReference 
+
+        return e1 
+
+class ChangeAssigneeAction(Action):
+    def __init__(self, takenAt, takenBy, newAssignee):
+        super().__init__(takenAt, takenBy, "changed_assignee")
+
+        self.newAssignee = newAssignee 
+
+    def toXML(self):
+        e1 = super().toXML()
+
+        e2 = XMLSubelement(e1, "new_assignee")
+        e2.text = self.newAssignee 
+
+        return e1 
+
+class ChangePriorityAction(Action):
+    def __init__(self, takenAt, takenBy, newPriority):
+        super().__init__(takenAt, takenBy, "changed_priority")
+
+        self.newPriority = newPriority 
+
+    def toXML(self):
+        e1 = super().toXML()
+
+        e2 = XMLSubelement(e1, "new_priority")
+        e2.text = self.newPriority 
+
+        return e1 
+
+class AddCommentAction(Action):
+    def __init__(self, takenAt, takenBy, commentReference, commentText):
+        super().__init__(takenAt, takenBy, "add_comment")
+
+        self.commentReference = commentReference
+        self.commentText = commentText 
+
+    def toXML(self):
+        e1 = super().toXML()
+
+        e2 = XMLSubelement(e1, "comment_reference")
+        e2.text = self.commentReference 
+
+        e3 = XMLSubelement(e1, "comment_text")
+        e3.text = self.commentText 
+
+        return e1 
