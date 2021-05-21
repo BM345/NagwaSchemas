7015f871cf54a09451aae8737a79bcc115f8904c
BM345 <40637208+BM345@users.noreply.github.com>
2021-05-21 14:46:43 +0100

    Added more to history code

--- history/python/history.py
+++ history/python/history.py
@@ -11,6 +11,35 @@ class History(object):
         self.state = State() 
         self.actions = []
 
+    def addAction(self, action, updateState = True):
+        self.actions.append(action)
+
+        if updateState:
+            if isinstance(action, CreateNewVersionAction):
+                self.state.versions.append(Version(action.reference, action.fileName))
+            if isinstance(action, ChangeWorkflowAction):
+                self.state.workflowReference = action.newWorkflowReference 
+            if isinstance(action, ChangeWorkflowStatusAction):
+                self.state.workflowStatusReference = action.newWorkflowStatusReference 
+            if isinstance(action, ChangeAssigneeAction):
+                self.state.assignee = action.newAssignee 
+            if isinstance(action, ChangePriorityAction):
+                self.state.priority = action.newPriority 
+            if isinstance(action, AddLabelAction):
+                self.state.labels.append(action.label) 
+            if isinstance(action, RemoveLabelAction):
+                self.state.labels.remove(action.label) 
+            if isinstance(action, AddWatcherAction):
+                self.state.watchers.append(action.watcher) 
+            if isinstance(action, RemoveWatcherAction):
+                self.state.watchers.remove(action.watcher)
+
+    def getComments(self):
+        actions = [action for action in self.actions if action.type == "added_comment"]
+        comments = [Comment(action.takenAt, action.takenBy, action.commentReference, action.commentText) for action in actions]
+
+        return comments 
+
     def toXML(self):
         e1 = XMLElement("history")
         e1.set("for_content_entity", "{}/{}".format(self.contentEntityType, self.contentEntityId))
@@ -95,9 +124,9 @@ class Developer(object):
         return e1 
 
 class Version(object):
-    def __init__(self):
-        self.reference = ""
-        self.fileName = ""
+    def __init__(self, reference = "", fileName = ""):
+        self.reference = reference
+        self.fileName = fileName
 
     def toXML(self):
         e1 = XMLElement("version")
@@ -105,6 +134,13 @@ class Version(object):
         e1.text = self.reference 
 
         return e1 
+
+class Comment(object):
+    def __init__(self, madeAt, madeBy, reference, text):
+        self.madeAt = madeAt 
+        self.madeBy = madeBy 
+        self.reference = reference 
+        self.text = text 
 
 class Action(object):
     def __init__(self, takenAt, takenBy, _type):
@@ -217,5 +253,61 @@ class AddCommentAction(Action):
 
         e3 = XMLSubelement(e1, "comment_text")
         e3.text = self.commentText 
+
+        return e1 
+
+class AddLabelAction(Action):
+    def __init__(self, takenAt, takenBy, label):
+        super().__init__(takenAt, takenBy, "add_label")
+
+        self.label = label
+
+    def toXML(self):
+        e1 = super().toXML()
+
+        e2 = XMLSubelement(e1, "label")
+        e2.text = self.label 
+
+        return e1 
+
+class RemoveLabelAction(Action):
+    def __init__(self, takenAt, takenBy, label):
+        super().__init__(takenAt, takenBy, "remove_label")
+
+        self.label = label
+
+    def toXML(self):
+        e1 = super().toXML()
+
+        e2 = XMLSubelement(e1, "label")
+        e2.text = self.label 
+
+        return e1 
+
+class AddWatcherAction(Action):
+    def __init__(self, takenAt, takenBy, watcher):
+        super().__init__(takenAt, takenBy, "add_watcher")
+
+        self.watcher = watcher
+
+    def toXML(self):
+        e1 = super().toXML()
+
+        e2 = XMLSubelement(e1, "watcher")
+        e2.text = self.watcher 
+
+        return e1 
+
+class RemoveWatcherAction(Action):
+    def __init__(self, takenAt, takenBy, watcher):
+        super().__init__(takenAt, takenBy, "remove_watcher")
+
+        self.watcher = watcher
+
+    def toXML(self):
+        e1 = super().toXML()
+
+        e2 = XMLSubelement(e1, "watcher")
+        e2.text = self.watcher 
 
         return e1 
